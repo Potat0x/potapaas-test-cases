d55ddd15ea356ae7a241bf9fdd6efd41dba4507b
Potat0x <Potat0x@users.noreply.github.com>
2019-09-15 16:10:15 +0200

    Test passes, start fails

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const port = process.env.PORT || 8080;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+throw 'boom';
