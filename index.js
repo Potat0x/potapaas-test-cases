2fb44c00994581d725c4a8134a7d89e3fe28165f
Potat0x <Potat0x@users.noreply.github.com>
2019-09-28 17:37:19 +0200

    DEPLOY_COMMIT_TEST_1

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const port = process.env.PORT || 8080;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+console.log("DEPLOY_COMMIT_TEST_1");
